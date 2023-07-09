fd65a1e3b9f841c8b67bf23c8913f2579e0526af
Akash-Tandale001 <ak.tandale123@gmail.com>
2023-07-09 23:20:37 +0530

    added chatgpt chatbot

--- yeild_smart/src/Components/Predict/Yeild.js
+++ yeild_smart/src/Components/Predict/Yeild.js
@@ -6,71 +6,99 @@ import swal from "sweetalert";
 import './yield.css'
 import { useNavigate } from "react-router-dom";
 import Navbar2 from "../Navbar/Navbar2";
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import {MetroSpinner} from 'react-spinners-kit'
+import Loader from "../../helper/Loader";
 
 const Yeild = () => {
-    const [error, seterror] = useState("");
-    const [loading, setLoading] = useState();
-    const onChange = (event) => {
-        setValues({ ...values, [event.target.name]: event.target.value });
-      };
-      let userData = sessionStorage.getItem("userData")
-      const navigate = useNavigate();
-      useEffect(()=>{
-        if(!userData){
-          navigate('/login')
+  const [error, seterror] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [result, setResult] = useState("");
+  const onChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  let userData = sessionStorage.getItem("userData")
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!userData) {
+      navigate('/login')
 
-        }
-      },[])
-      const [values, setValues] = useState({
-        nitrogen: "",
-        phosphorus: "",
-        potassium: "",
-        temperature: "",
-        humidity: "",
-        ph: "",
-        rainfall: "",
-      });
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true)
-        if(values.nitrogen === '')
-        { seterror("*Nitrogen is Required!");}
-        else if(values.phosphorus === '')
-        { seterror("*Phosphorus is Required!");}
-        else if(values.potassium === '')
-        { seterror("*Potassium is Required!");}
-        else if(values.temperature === '')
-        { seterror("*Temperature is Required!");}
-        else if(values.humidity === '')
-        { seterror("*Humidity is Required!");}
-        else if(values.ph === '')
-        { seterror("*Ph is Required!");}
-        else if(values.rainfall === '')
-        { seterror("*Rainfall is Required!");}
-        else
-    {
+    }
+  }, [])
+  const handleSearchChange = (event) => {
+    const value = event.target.value;
+    setSearch(value);
+  }
+  const handleOnSearch = async () => {
+    setLoading(true)
+    const res = await axios.post(`http://3.88.181.187:8080/v1/`, {
+      "model": "gpt-4",
+      "messages": [{ "role": "user", "content": `${search}` }]
+    })
+    console.log('res', JSON.stringify(res))
+    setResult(res.data.choices[0].message.content)
+    setSearch("");
+    setLoading(false)
+  }
+  const [values, setValues] = useState({
+    nitrogen: "",
+    phosphorus: "",
+    potassium: "",
+    temperature: "",
+    humidity: "",
+    ph: "",
+    rainfall: "",
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true)
+    if (values.nitrogen === '') { seterror("*Nitrogen is Required!"); }
+    else if (values.phosphorus === '') { seterror("*Phosphorus is Required!"); }
+    else if (values.potassium === '') { seterror("*Potassium is Required!"); }
+    else if (values.temperature === '') { seterror("*Temperature is Required!"); }
+    else if (values.humidity === '') { seterror("*Humidity is Required!"); }
+    else if (values.ph === '') { seterror("*Ph is Required!"); }
+    else if (values.rainfall === '') { seterror("*Rainfall is Required!"); }
+    else {
       const { data } = await axios.post(`https://yeildsmart.onrender.com/predictCrop`, { nitrogen: Number(values.nitrogen), phosphorus: Number(values.phosphorus), potassium: Number(values.potassium), temperature: Number(values.temperature), humidity: Number(values.humidity), ph: Number(values.ph), rainfall: Number(values.rainfall) })
       setLoading(false)
       seterror("");
-      values.nitrogen='';values.phosphorus='';values.potassium='';values.temperature='';values.humidity='';values.ph='';values.rainfall='';
+      values.nitrogen = ''; values.phosphorus = ''; values.potassium = ''; values.temperature = ''; values.humidity = ''; values.ph = ''; values.rainfall = '';
       swal("Success", `You should plant ${data.result} in your field`, "success");
-    }     
+    }
   }
-	return (
+  return (
     <>
-    <Navbar2/>
-	  <div className="body">
-		<form onSubmit={handleSubmit}>
-		  <h1 className='title'>Crop Recomendation</h1>
-		  {error && (<p style={{color:"red"}}>{error}</p>)}
-		  {inputs.map((input) => (
-			<Predict key={input.id} {...input} value={values[input.name]} onChange={onChange}
-			/>
-		  ))}
-		  <button onClick={handleSubmit} className='btn'>{loading ? 'Evaluating...' : 'Recommend Crop'}</button>
-		</form>
-	  </div></>)
-  }
+      <Navbar2 />
+      <div className="body">
+        <div>
+          <form onSubmit={handleSubmit}>
+            <h1 className='title'>Crop Recomendation</h1>
+            {error && (<p style={{ color: "red" }}>{error}</p>)}
+            {inputs.map((input) => (
+              <Predict key={input.id} {...input} value={values[input.name]} onChange={onChange}
+              />
+            ))}
+            <button onClick={handleSubmit} className='btn'>{loading ? 'Evaluating...' : 'Recommend Crop'}</button>
+          </form>
+        </div>
+        <div style={{width:"50%",alignSelf:"start"}}>
+          <form onSubmit={handleSubmit} style={{height:"60em",overflow:"scroll"}}>
+            <h1 className='title'>Chat with expert</h1>
+            <div style={{ display: "flex" }}>
+              <input name="search" value={search} onChange={handleSearchChange} style={{ width: "40em" }} placeholder="Ask Any Question" />
+              <div >
+                <ArrowCircleRightIcon style={{ fontSize: "45px", marginTop: "9px", marginLeft: "10px", cursor: "pointer" }} onClick={handleOnSearch} />
+              </div>
+            </div>
+            <div style={{marginTop:"1.5em",fontSize:"1.3em"}}>
+              {result==="" ? loading ? <Loader/>:null:result}
+            </div>
+          </form>
+        </div>
+      </div></>)
+}
 
-  export default Yeild;
+export default Yeild;
